ce77a100e326b32f34908470f26c852dddfcfff3
yeabnoah <yeabnoah5@gmail.com>
2025-01-29 16:45:44 +0300

    initialize the project with turborepo and starter folders for the project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,7 +9,6 @@ type Props = Omit<ImageProps, "src"> & {
 
 const ThemeImage = (props: Props) => {
   const { srcLight, srcDark, ...rest } = props;
-
   return (
     <>
       <Image {...rest} src={srcLight} className="imgLight" />
